23ca5cb5b622c2acc852e209185ff3a468a628ad
DosImpact <ypd03008@gmail.com>
2021-01-24 02:08:55 +0900

    ApolloProvider

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className=" text-xl text-white">
+        <h1 className=" bg-green-300">hello there</h1>
+      </header>
     </div>
   );
 }
