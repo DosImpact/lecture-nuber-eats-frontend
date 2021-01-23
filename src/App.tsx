6edbd5090fc50d0e1c81ece3e8b9626be586870f
DosImpact <ypd03008@gmail.com>
2021-01-24 02:14:29 +0900

    react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+import LoggedOutRouter from "./routers/logged-out-router";
 
 function App() {
   return (
     <div className="App">
       <header className=" text-xl text-white">
         <h1 className=" bg-green-300">hello there</h1>
+        <LoggedOutRouter />
       </header>
     </div>
   );
